81e00a6594f7794a6d56b41c71a53dea0e6578d2
LoeiFy <LoeiFy@gmail.com>
2020-07-27 09:08:05 +0800

    feat: layout

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react'
+import React from 'react'
 import GridLayout, { WidthProvider } from 'react-grid-layout'
 import { useStore } from 'nycticorax'
 import classes from './index.module.less'
@@ -8,47 +8,30 @@ const RGL = WidthProvider(GridLayout)
 
 export default function () {
   const { components } = useStore('components')
-  const [selects, setSelects] = useState([])
 
-  const layout = selects.map((name, i) => ({
+  const layout = components.map(({ name }, i) => ({
     i: name,
-    x: i * 2,
-    y: 0,
-    w: 2,
-    h: 2,
+    x: i * 4 % 12,
+    y: Infinity,
+    w: 4,
+    h: 4,
   }))
 
-  const onSelect = useCallback((name) => {
-    if (!selects.includes(name)) {
-      selects.push(name)
-      setSelects(selects.slice())
-    }
-  }, [selects])
-
-  const onDelete = useCallback((name) => {
-    const current = selects.filter((item) => item !== name)
-    setSelects(current)
-  }, [selects])
-
   return (
-    <>
-      <div className={classes.box}>
-        {
-          components.map(({ name }) => (
-            <div key={name} onClick={() => onSelect(name)}>{name}</div>
-          ))
-        }
-      </div>
-      <RGL layout={layout} cols={12} rowHeight={30} width={1200}>
-        {
-          selects.map((name) => (
-            <div key={name}>
-              <div onClick={() => onDelete(name)}>x</div>
-              {name}
-            </div>
-          ))
-        }
-      </RGL>
-    </>
+    <RGL
+      onLayoutChange={(layout) => {
+        console.log(layout)
+      }}
+      layout={layout}
+      cols={12}
+      rowHeight={30}
+      width={1200}
+    >
+      {
+        layout.map(({ i }) => (
+          <div key={i}>{i}</div>
+        ))
+      }
+    </RGL>
   )
 }
