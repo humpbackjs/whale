1af7a694b912f5df7df27d09d353ff38731b17a2
LoeiFy <LoeiFy@gmail.com>
2020-08-07 09:22:30 +0800

    feat: modal

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,16 +1,28 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, useLocation, useHistory } from 'react-router-dom'
+import { Modal } from 'antd'
 import routes from '../../routes'
 
 export default function () {
+  const { pathname } = useLocation()
+  const history = useHistory()
+  const { name } = routes.find((item) => item.path === pathname) || {}
+
   return (
-    <div>
+    <Modal
+      title={name}
+      visible={!!name}
+      footer={null}
+      centered
+      width="78%"
+      onCancel={history.goBack}
+    >
       {
         routes
           .map(({ path, component }) => (
             <Route key={path} exact path={path} component={component} />
           ))
       }
-    </div>
+    </Modal>
   )
 }
